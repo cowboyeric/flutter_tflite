ef2d9f9bc2c4e3389677e34516c62eafaab5742b
Eric <>
2022-11-11 23:43:07 +0800

    turn clean color

--- lib/tflite.dart
+++ lib/tflite.dart
@@ -254,7 +254,7 @@ class Tflite {
 
   // https://github.com/meetshah1995/pytorch-semseg/blob/master/ptsemseg/loader/pascal_voc_loader.py
   static List<int> pascalVOCLabelColors = [
-    Color.fromARGB(255, 0, 0, 0).value, // background
+    Color.fromARGB(0, 0, 0, 0).value, // background
     Color.fromARGB(255, 128, 0, 0).value, // aeroplane
     Color.fromARGB(255, 0, 128, 0).value, // biyclce
     Color.fromARGB(255, 128, 128, 0).value, // bird
